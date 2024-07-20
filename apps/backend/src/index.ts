b1513027334589d02720efc606a72473af868993
theVicoH <huang.victor.mail@gmail.com>
2024-07-20 15:18:40 +0200

    back: Connect to db

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config({ path: "../../.env" });
 
 const app = express();
-const port = process.env.BACKEND_PORT || 3000;
-console.log(process.env.BACKEND_PORT)
+const port = process.env.EXPRESS_PORT || 3000;
+console.log(process.env.BACKEND_PORT);
+
+const mongoURI = process.env.MONGODB_URI || "mongodb+srv://vicoh:vicoh@cluster0.zjgtlhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as mongoose.ConnectOptions);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
